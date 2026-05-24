9612d7d1ed0be45e84515de15107c38ef64415e1
griffithazrael <mrifqifauzan@gmail.com>
2026-05-24 18:04:38 +0700

    feat/Added background element, changed text color

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ function App() {
   return (
     <>
       {/* Header */}
-      <div className='flex w-full px-20 py-5 fixed justify-between items-center bg-black/20'>
-        <img src="Haybreakers-logo.png" alt="Logo" className='h-10' />
-        <div className='flex items-center gap-10'>
-          <button>What I Do</button>
-          <button>What I Don't Do</button>
+      <div className='flex w-full px-20 py-5 fixed justify-between items-center bg-black/20 z-50'>
+        <img src='Haybreakers-logo.png' alt='Logo' className='h-10' />
+        <div className='flex items-center gap-10 text-white'>
+          <button>About</button>
+          <button>Skill</button>
+          <button>Projects</button>
+          <button>FAQ</button>
+          <button>Contact</button>
         </div>
       </div>
       {/* Main Content */}
-      <div className='px-20 pt-20'>
-        <h1 className='font-bold'>Test</h1>
+      <div className="min-h-screen bg-[url('desert.jpg')] bg-cover bg-center bg-fixed">
+        <div className='min-h-screen bg-black/25'>
+        <div className='px-20 pt-24 z-10 text-white'>
+          <h1 className='font-bold'>Test</h1>
+        </div>
+        </div>
       </div>
       {/* Footer */}
     </>
